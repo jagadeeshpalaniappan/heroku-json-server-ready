03a37f19c0b9e22060feaf693e039a2ca0504186
Jagadeesh <jaganttp@gmail.com>
2016-10-19 17:52:05 -0700

    first push

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
 var jsonServer = require('json-server');
-var path = require("path");
-
-
-
-var dbFile = path.join(__dirname, 'db.json');
-console.log(dbFile);
-
-
-//Server-Config ::
-
 var server = jsonServer.create();
-var router = jsonServer.router(dbFile);
+var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
-
-
-
-// Set default middlewares (logger, static, cors and no-cache)
+var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
-
-
-// Add custom routes before JSON Server router
-server.get('/echo', function (req, res) {
-    res.jsonp(req.query);
-});
-
-
-server.use(function (req, res, next) {
-    if (req.method === 'POST') {
-        req.body.createdAt = Date.now();
-    }
-    // Continue to JSON Server router
-    next();
-});
-
-
-// Use default router
 server.use(router);
-
-var PORT = (process.env.PORT || 5000);
-
-server.listen(PORT, function () {
-    console.log('RETS-API ::is running at port: '+ PORT);
+server.listen(port, function () {
+    console.log('JSON Server is running')
 });
